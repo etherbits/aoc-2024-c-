b6bb5854e52f803573b4ddc23ab5222957f2146d
Nika Qvrivishvili <nika.qvrivishviliPC@gmail.com>
2024-12-03 05:21:10 +0400

    d2

--- aoc-2024-cs/Program.cs
+++ aoc-2024-cs/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using aoc_2024_cs.d1;
+﻿using aoc_2024_cs.d1;
 
 Main();
 return;
@@ -11,6 +9,7 @@ void Main(){
         Console.WriteLine("Usage: aoc-2024-cs <day> <part>");
         return;
     }
+    
     
     var q = args[1] + "-"+args[2];
 
@@ -23,6 +22,14 @@ void Main(){
         case "1-2":
             Console.WriteLine("Running d1 p2:");
             D1.P2();
+            break;
+        case "2-1":
+            Console.WriteLine("Running d2 p1:");
+            D2.P1();
+            break;
+        case "2-2":
+            Console.WriteLine("Running d2 p2:");
+            D2.P2();
             break;
         default:
             Console.WriteLine("No problem found for day {0} part {1}", args[1], args[2]);
---
+++ aoc-2024-cs/d2/D2.cs
@@ -0,0 +1,66 @@
+using aoc_2024_cs.utils;
+
+namespace aoc_2024_cs.d1;
+
+public static class D2
+{
+    public static void P1()
+    {
+        var lines = Helpers.GetLines("/d2/p1.txt");
+        var goodCount = lines.Sum(line => IsGood(line) ? 1 : 0);
+
+        Console.WriteLine(goodCount);
+    }
+
+    public static void P2()
+    {
+        var lines = Helpers.GetLines("/d2/p2.txt");
+        var goodCount = lines.Sum(line => IsGoodTol(line) ? 1 : 0);
+
+        Console.WriteLine(goodCount);
+    }
+
+    private static bool IsGood(string line)
+    {
+        var nums = line.Split(" ").Select(int.Parse).ToList();
+        var dir = nums[1] - nums[0] > 0 ? 1 : -1;
+
+        for (var i = 0; i < nums.Count - 1; i++)
+        {
+            var change = nums[i + 1] - nums[i];
+            if (IsBadChange(change, dir)) return false;
+        }
+
+        return true;
+    }
+
+    private static bool IsGoodTol(string line)
+    {
+        var nums = line.Split(" ").Select(int.Parse).ToList();
+        var numPerms = nums.Select((num, i) =>
+        {
+            var newNums = new List<int>(nums);
+            newNums.RemoveAt(i);
+            return newNums;
+        }).ToList();
+        numPerms.Add(nums);
+
+        return numPerms.Any(numPerm =>
+        {
+            var dir = numPerm[1] - numPerm[0] > 0 ? 1 : -1;
+            for (var i = 0; i < numPerm.Count - 1; i++)
+            {
+                var change = numPerm[i + 1] - numPerm[i];
+
+                if (IsBadChange(change, dir)) return false;
+            }
+
+            return true;
+        });
+    }
+
+    private static bool IsBadChange(int change, int dir)
+    {
+        return change * dir <= 0 || Math.Abs(change) < 1 || Math.Abs(change) > 3;
+    }
+}
